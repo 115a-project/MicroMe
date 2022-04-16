c2e9599d8a3bead8332213853d1ca9e2b63cd8e8
Kimi Holsapple <65469416+KimiHolsapple@users.noreply.github.com>
2022-04-15 20:03:29 -0700

    Update _navigationBar.dart
    This links to the nav bar class

--- testing/lib/_navigationBar.dart
+++ testing/lib/_navigationBar.dart
@@ -1,58 +1,90 @@
-// SRC @ https://www.youtube.com/watch?v=18PVdmBOEQM
+
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/water.dart';
-import 'package:flutter_application_1/steps.dart';
-import 'package:flutter_application_1/journal.dart';
+import 'package:flutter_application_1/_navigationBar.dart';
 
-class bottom_nav_bar extends StatefulWidget {
-  // const ({ Key? key }) : super(key: key);
-
-  @override
-  _navigationBar createState() => _navigationBar();
+// main to run 
+void main() {
+  runApp(const MyApp());
 }
 
-class _navigationBar extends State<bottom_nav_bar> {
-
-  // determines which nav_child is being picked: water, journal, ...
-  int _currentIndex = 0;
-  final List<Widget> _nav_bar_children = 
-  [
-    water(),
-    steps(),
-    journal(),
-  ];
-
-  // Allows tapping fucntionality
-  void onTappedBar(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) 
-  {
-    return new Scaffold(
-      body: _nav_bar_children[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar (
-
-        onTap: onTappedBar, currentIndex: _currentIndex, 
-
-        items: [
-          BottomNavigationBarItem(
-            label: "Water",
-            icon: Icon(Icons.water_drop), 
-            
-          ),
-          BottomNavigationBarItem(
-            label: "Steps",
-            icon: Icon(Icons.run_circle_outlined), ),
-          BottomNavigationBarItem(
-            label: "Journal",
-            icon: Icon(Icons.book), ),
-        ],
-        type: BottomNavigationBarType.fixed,
-      )
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      title: 'Welcome to Flutter!',
+      home: MyHomePage(),
     );
-  } 
+  }
 }
+
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text('Welcome to the Homepage'),
+          actions: <Widget> [
+            IconButton(
+              icon: const Icon(Icons.settings),
+              tooltip: 'Settings',
+              onPressed: () {},
+            ),
+          ],
+          leading: IconButton(
+            icon: const Icon(Icons.home),
+            tooltip: 'Return to homepage',
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Screen2()));
+            },
+          )
+        ),
+
+        body: const Center(
+          child: Text('Welcome!')
+        ),
+        bottomNavigationBar: bottom_nav_bar(),
+    );
+  }
+}
+
+class Screen2 extends StatefulWidget {
+  const Screen2({Key? key}) : super(key: key);
+
+  @override
+  _Screen2State createState() => _Screen2State();
+}
+class _Screen2State extends State<Screen2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text('Navigate to a new screen on Button click'),
+          leading: IconButton(
+            icon: const Icon(Icons.redo),
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (context)=>MyHomePage()));
+            },
+          ),
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: <Color>[
+                  Colors.blue,
+                  Colors.green
+                ]
+              ),
+            ),
+          ),
+      ),
+    );
+  }
+}
+
+
