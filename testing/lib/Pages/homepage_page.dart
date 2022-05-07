5078f90e3ac44a0da9701f90046d0d3bb9366890
Kimi Holsapple <65469416+KimiHolsapple@users.noreply.github.com>
2022-05-07 12:12:31 -0700

    Update homepage_page.dart
    UI scaffold

--- testing/lib/Pages/homepage_page.dart
+++ testing/lib/Pages/homepage_page.dart
@@ -11,7 +11,34 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Text("Home Page"),
+      body: Container(
+        child: SingleChildScrollView(
+          child: 
+            Column(
+              children: <Widget> [
+                // @Niko I made a place for you to put motivational messages here :)
+                Container(
+                  
+                ),
+                // Statistics entries //
+                Container( 
+                  margin: const EdgeInsets.all(30),
+                  child: const Text("Water Statistics "),
+                ),
+                Container( 
+                  margin: const EdgeInsets.all(30),
+                  child: const Text("Step Statistics "),
+                ),
+                Container( 
+                  margin: const EdgeInsets.all(30),
+                  child: const Text("Your Week Long Streak "),
+                ),
+                
+              ],
+          ),
+
+        )
+      ),
     );
   }
 }
