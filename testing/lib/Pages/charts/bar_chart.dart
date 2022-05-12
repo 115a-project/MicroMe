d58b13a37392969897bef748107d22c211157a3f
Kimi Holsapple <kimiholsapple@gmail.com>
2022-05-12 11:18:13 -0700

    chart.1

---
+++ testing/lib/Pages/charts/bar_chart.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+
+class WaterChart {
+  // Chart statistics variables and controllers //
+  String day;
+  int amount;
+  WaterChart(this.day, this.amount);
+}
+
---
+++ testing/lib/Pages/charts/water_data.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:testing/Pages/charts/chart_data.dart'; 
+
+class BarData {
+    static int interval = 5;
+
+  static List<Data> barData = [
+    Data(
+      id: 0,
+      name: "Mon",
+      y: 15,
+      color: Color(0xff19bfff),
+    ),
+  ];
+
+}
