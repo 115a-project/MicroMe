c95db227d340db8410294f372f490b418d9413be
Niko Vanden Heuvel <nvandenh@ucsc.edu>
2022-05-16 20:38:12 -0700

    quotetesting

--- testing/lib/Pages/homepage_page.dart
+++ testing/lib/Pages/homepage_page.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:testing/Utils/quote.dart';
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -9,8 +12,45 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Home Page"),
+    return Scaffold(
+        body: SingleChildScrollView(
+          child:
+          FutureBuilder(
+              future: fetchQuote(),
+              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return ListTile(
+                    title: Text(
+                      snapshot.data[1].author,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(snapshot.data[1].text),
+                  );
+                }
+                else {
+                  return const Center(
+                    child: CircularProgressIndicator(color: Colors.amber),
+                  );
+                }
+              }
+          ),
+        )
     );
+  }
+}
+
+Future fetchQuote() async {
+  final response = await http.get(Uri.parse('https://favqs.com/api/qotd'));
+  if (response.statusCode == 200) {
+    var jsonData = json.decode(response.body);
+    var quotes = [];
+    for (var aQuote in jsonData) {
+      Quote quote = Quote(
+          text: aQuote['text'], author: aQuote['author'] ?? "unknown");
+      quotes.add(quote);
+    }
+    return quotes;
+  } else {
+    throw Exception('Failed to load Quote');
   }
 }
