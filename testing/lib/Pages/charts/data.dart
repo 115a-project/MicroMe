4801e59aa417a2d7eda6f4000ea7372430c42094
Kimi Holsapple <kimiholsapple@gmail.com>
2022-05-25 18:47:40 -0700

    hhh I hjope this fixed the cant find package error

---
+++ testing/lib/Pages/charts/data.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+
+// Define data structure for a bar group
+class Data {
+  int x;
+  double y1;
+  Data({required this.x, required this.y1});
+}
+
+// class Data {
+//   int id;
+//   String name;
+//   double y;
+//   Color color;
+
+//   Data( this.id,  this.name,  this.y,  this.color );
+
+//   // Data.fromMap(Map<String.dynamic> map)
+//   // :assert(map['id']!=null),
+//   //  assert(map['name']!=null),
+//   //  assert(map['y']!=null),
+//   //  assert(map['color']!=null),
+//   //   id=map['id'],
+//   //   name=map['name'],
+//   //   y=map['y'],
+//   //   color=map['color'];
+
+// }
